8f497c598ef67ab85f0180ed43540f54526557a1
Alexey Belkov <lexabelco2013@yandex.ru>
2023-03-12 02:59:35 +0500

    feat: подготовить стартовый шаблон проекта

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
