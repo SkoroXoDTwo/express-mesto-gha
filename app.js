5aa19fc7144801134be8376990c4c0a40a506286
Alexey Belkov <lexabelco2013@yandex.ru>
2023-03-12 22:27:38 +0500

    feat: добавить схему и модель user

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const app = express();
-
-mongoose.connect('mongodb://localhost:27017/mestodb');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '640e085b2dade0952b919b76',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
