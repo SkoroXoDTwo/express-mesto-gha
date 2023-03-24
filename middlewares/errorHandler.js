6a47eda0860e75293a36b94f90de60f5485c6dfa
Alexey Belkov <lexabelco2013@yandex.ru>
2023-03-24 22:33:12 +0500

    feat: исправить ошибки после ревью

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -9,35 +9,5 @@ module.exports = (err, req, res, next) => {
         : message,
     });
 
-  // if (err.code === 11000) {
-  //   res.status(409).send({ message: 'Пользователь с таким email уже зарегестрирован' });
-  //   return;
-  // }
-
-  // switch (err.name) {
-  //   case 'CastError':
-  //     res.status(400).send({ message: 'Переданы некорректные данные' });
-  //     break;
-
-  //   case 'ValidationError':
-  //     res.status(400).send({ message: 'Переданы некорректные данные' });
-  //     break;
-
-  //   case 'DataNotFoundError':
-  //     res.status(404).send({ message: err.message });
-  //     break;
-
-  //   case 'TokeIsIncorrectError':
-  //     res.status(401).send({ message: err.message });
-  //     break;
-
-  //   case 'InvalidAuthorizationData':
-  //     res.status(401).send({ message: err.message });
-  //     break;
-
-  //   default:
-  //     res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
-  // }
-
   next();
 };
