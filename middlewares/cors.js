444d94cc6f9f6da6e1585c6e4d6a1ac32d8917fc
Alexey Belkov <lexabelco2013@yandex.ru>
2023-03-30 01:38:49 +0500

    fix: поправить обработку cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,8 @@
 const allowedCors = [
   'https://mesto.skoroxod.nomoredomains.work',
   'http://mesto.skoroxod.nomoredomains.work',
-  'localhost:3000',
+  'https://localhost:3000/',
+  'http://localhost:3000/',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
